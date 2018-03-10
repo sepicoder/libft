5b1ce9eef7bf880568d5c8aa8d28e55e0d12db25
Sepideh Homami <shomami@e1z2r9p3.42.us.org>
2018-03-09 21:16:30 -0800

    dkd

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: shomami <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 18:14:32 by shomami           #+#    #+#             */
-/*   Updated: 2018/03/09 20:55:22 by shomami          ###   ########.fr       */
+/*   Updated: 2018/03/09 21:12:40 by shomami          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_strndup(const char *str, size_t len)
 {
 	char	*cpy;
-	
+
 	if (!(cpy = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	ft_strncpy(cpy, str, len);
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: shomami <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 21:47:37 by shomami           #+#    #+#             */
-/*   Updated: 2018/03/09 20:54:24 by shomami          ###   ########.fr       */
+/*   Updated: 2018/03/09 21:13:51 by shomami          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static int	ft_wordcount(char const *str, char c)
 {
-	int words;
+	int		words;
 
 	words = 0;
 	while (*str)
@@ -29,37 +29,28 @@ static int	ft_wordcount(char const *str, char c)
 	return (words);
 }
 
-char	**ft_strsplit(char const *s, char c)
+char		**ft_strsplit(char const *s, char c)
 {
-	char 	**arr;
+	char	**arr;
 	int		words;
-	int		i;
 	int		letters;
 	int		current;
 
-	current = 0;
-	letters = 0;
-	i = 0;
-	if (!s || !c)
+	if (!s)
 		return (NULL);
 	words = ft_wordcount(s, c);
 	if (!(arr = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c) + 1))))
 		return (NULL);
-	arr[words] = 0;
-	while (s[i])
+	current = 0;
+	while (words--)
 	{
-		while (s[i] == c && s[i])
-			i++;
-		if (s[i] != c && s[i])
-		{
-			letters = 0;
-			while (s[i] != c && s[i])
-			{
-				letters++;
-				i++;
-			}
-			arr[current++] = ft_strndup(&s[i - letters], letters);
-		}
+		while (*s == c && *s)
+			s++;
+		letters = 0;
+		while (s[letters] != c && s[letters])
+			letters++;
+		arr[current++] = ft_strndup(s, letters);
+		s += letters;
 	}
 	arr[current] = NULL;
 	return (arr);
